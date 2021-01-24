daf98e383d5eaee76c540a72330295648a7853db
parmis <hellboyh886@yahoo.com>
2021-01-24 05:19:16 -0800

    range(10)

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-
-name = 'farshad rahimi'
-print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+print("Printing current and previous number sum in a given range(10)")
+previous_Number = 0
+sum = 0
+for num in range (1 ,11):
+    print("Current Number {} Previous Number {}  Sum: {}".format(num-1, previous_Number, sum))
+    previous_Number = num -1
+    sum =num + previous_Number
